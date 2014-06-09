1d1b6dc1ad695601a776a2cee9e4879196ca92ab
Alessio Caiazza <nolith@abisso.org>
2014-06-09 18:10:44 +0200

    detect ANATHEMA_DIR on linux

--- Development_Distribution/Linux/anathema.sh
+++ Development_Distribution/Linux/anathema.sh
@@ -1,6 +1,12 @@
 #!/bin/sh
 
-ANATHEMA_DIR=/usr/share/anathema
+SOURCE="${BASH_SOURCE[0]}"
+while [ -h "$SOURCE" ]; do # resolve $SOURCE until the file is no longer a symlink
+  DIR="$( cd -P "$( dirname "$SOURCE" )" && pwd )"
+  SOURCE="$(readlink "$SOURCE")"
+  [[ $SOURCE != /* ]] && SOURCE="$DIR/$SOURCE" # if $SOURCE was a relative symlink, we need to resolve it relative to the path where the symlink file was located
+done
+ANATHEMA_DIR="$( cd -P "$( dirname "$SOURCE" )" && pwd )"
 REPO_DIR=${HOME}/.anathema/repository
 JAVA_BIN=
 
